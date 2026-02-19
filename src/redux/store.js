4956d2c6011e05b4d45a32c9434f7faff1ca215a
youminsu0523 <youminsu0523@gmail.com>
2026-02-19 13:24:30 +0900

    feat: Initialize Redux store with auth, modal, and api reducers.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import modalReducer from './slices/modalSlice';
-import apiReducer from './slices/apiSlice';
+import authReducer from './slices/apiSlice';
 
 const store = configureStore({
   reducer: combineReducers({
     auth: authReducer,
     modal: modalReducer,
-    api: apiReducer,
+    api: authReducer,
   }),
 });
 
